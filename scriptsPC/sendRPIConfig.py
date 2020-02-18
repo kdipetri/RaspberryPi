f1348e795dacbcf895d92079d29a75e6a84d6c97
mhussainphys <mhussainphys@uchicago.edu>
2020-02-18 16:39:32 -0600

    less ambiguity on status.txt

--- scriptsPC/sendRPIConfig.py
+++ scriptsPC/sendRPIConfig.py
@@ -9,23 +9,27 @@ def sendRPIConfig(config_number):
     # Raspberry Pi IP address and filepath
     pi_path="pi@192.168.133.4:~/Documents"
     
-    # Setup File to Send
+    # Setup Files to Send
     config_file = "config.txt"
     f=open(config_file,"w")
     f.write(config_number)
     f.close()
+
+    f=open("status.txt","w")
+    f.close()
     
-    # Send config number to RPI
+    # Send status file to RPI
+    bash_command = "scp status.txt {}/.".format(pi_path) 
+    
+    process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
+    for output in process.stdout.readlines():
+        print(output.strip())
+
     bash_command = "scp {} {}/.".format(config_file,pi_path) 
-    
-    process = subprocess.Popen(bash_command.split(), 
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE)
-    out,err = process.communicate()
-    print(out)
-    
-    #for output in process.stdout.readlines():
-    #    print(output.strip())
+
+    process = subprocess.Popen(bash_command.split(), stdout=subprocess.PIPE)
+    for output in process.stdout.readlines():
+        print(output.strip())
     
     # Wait to make sure config is picked up
     time.sleep(3)
